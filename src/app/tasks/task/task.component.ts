c65561c16f0053ece552ad5a1b7b61be65c4cd78
natsuo26 <abhaydkumar80@gmail.com>
2024-06-08 20:25:19 +0530

    completed module 2 all changes from module 2

--- src/app/tasks/task/task.component.ts
+++ src/app/tasks/task/task.component.ts
@@ -1,20 +1,22 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, Input, inject } from '@angular/core';
 
+import { TasksService } from '../tasks.service';
 import { type Task } from './task.model';
+import { CardComponent } from "../../shared/card/card.component";
+import { DatePipe } from '@angular/common';
 
 @Component({
     selector: 'app-task',
     standalone: true,
-    imports: [],
     templateUrl: './task.component.html',
-    styleUrl: './task.component.css'
+    styleUrl: './task.component.css',
+    imports: [CardComponent,DatePipe]
 })
 
 export class TaskComponent {
     @Input({required:true}) task!:Task;
-    @Output() complete=new EventEmitter<string>();
-    
+    private tasksService = inject(TasksService);
     onCompleteTask(){
-        this.complete.emit(this.task.id);
+        this.tasksService.removeTask(this.task.id);
     }
 }
